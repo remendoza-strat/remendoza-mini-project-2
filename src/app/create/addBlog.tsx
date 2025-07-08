b327c812fdf8eaa7d1036bd60d68a05db3bd3e85
Rhabi Mendoza <i_rhabiezra.mendoza@stratpoint.com>
2025-07-08 17:36:18 +0800

    create blog functionality

--- src/app/create/addBlog.tsx
+++ src/app/create/addBlog.tsx
@@ -3,24 +3,29 @@ import {db} from '@/db/drizzle';
 import {tbl_blog} from '@/db/schema';
 
 export async function addBlog(form : FormData){
-    const title = String(form.get("title"));
-    const imageUrl = String(form.get("imageUrl"));
-    const content = String(form.get("content"));
-    const author = String(form.get("author"));
-    const code = String(form.get("code"));
+    // get contents from form
+    const title = String(form.get("title")).trim();
+    const imageUrl = String(form.get("imageUrl")).trim() || "none";
+    const content = String(form.get("content")).trim();
+    const author = String(form.get("author")).trim() || "anonymous";
+    const code = String(form.get("code")).trim() || "none";
 
-    // logic for validating inputs
+    // return 2 for empty required fields
+    if(!title || !content){
+        return {status: 2};
+    }
 
     try{
+        // insert data to database
         await db   
             .insert(tbl_blog)
             .values({title, imageUrl, content, author, code});
 
-        // trigger a toast
-        // go to index
+        // return 1 for successful insertion of data
+        return {status: 1};
     }
-    catch(error){
-        // logic for displaying error
-        console.log(error);
+    catch{
+        // return 3 for error
+        return {status: 3}; 
     }
 }
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,15 +1,45 @@
-import {addBlog} from './addBlog';
+'use client';
+import {toast} from 'sonner';
+import {addBlog} from '@/app/create/addBlog';
 
 export default function Create(){
+    async function handleSubmit(event : React.FormEvent<HTMLFormElement>){
+        // prevent default and initialize variables
+        event.preventDefault();
+        const form = event.currentTarget;
+        const formData = new FormData(form);
+
+        try{
+            // get result of addBlog
+            const result = await addBlog(formData);
+
+            // display toast depending on status
+            if(result.status === 1){
+                toast.success("Blog created successfully!");
+                form.reset();
+            }
+            else if(result.status === 2){
+                toast.error("Please complete required fields.");
+            }
+            else{
+                toast.error("Error occured in creating the blog.");
+            }
+        }
+        catch{
+            // display error
+            toast.error("Error occured.");
+        }
+    }
+
     return(
         <>
-            <form action={addBlog}>
-                <input name="title" autoComplete="off" required placeholder="Title..."></input>
-                <input name="imageUrl" autoComplete="off" required placeholder="Image Url..."></input>
-                <textarea name="content" autoComplete="off" required placeholder="Content..."></textarea>
-                <input name="author" autoComplete="off" required placeholder="Author..."></input>
-                <input name="code" autoComplete="off" required placeholder="Code..."></input>
-                <button>Create Blog</button>
+            <form onSubmit={handleSubmit}>
+                <input name="title" autoComplete="off" placeholder="Title..."></input>
+                <input name="imageUrl" autoComplete="off" placeholder="Image Url..."></input>
+                <textarea name="content" autoComplete="off" placeholder="Content..."></textarea>
+                <input name="author" autoComplete="off" placeholder="Author..."></input>
+                <input name="code" autoComplete="off" placeholder="Code..."></input>
+                <button type="submit">Create Blog</button>
             </form>
         </>
     );
