3e0a561cb3034f276b254892eef6da6fec9e72c1
Rhabi Mendoza <i_rhabiezra.mendoza@stratpoint.com>
2025-07-14 00:35:28 +0800

    fix config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config';
-import {defineConfig} from 'drizzle-kit';
 
-export default defineConfig({
-    out: './drizzle',
-    schema: './src/db/schema.ts',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
-});
+const config = {
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+};
+
+export default config;
